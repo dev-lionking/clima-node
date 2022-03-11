ea7fbc0e141ca3fdbadee2461537d9d54fc3797c
Felipe Coll <devphilipcoll@gmail.com>
2022-03-11 08:37:02 -0300

    App clima hecha con Node

---
+++ index.js
@@ -0,0 +1,30 @@
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
+
+const main = async () => {
+  const busquedas = new Busquedas();
+  let opt;
+
+  do {
+    opt = await inquirerMenu();
+
+    switch (opt) {
+      case 1:
+        const lugar = await leerInput("Ciudad: ");
+        console.log(lugar);
+
+        console.log("\nInformación de la ciudad\n".green);
+        console.log("Ciudad");
+        console.log("Lat:");
+        console.log("Lng:");
+        console.log("Temperatura:");
+        console.log("Max:");
+        console.log("Min:");
+        break;
+    }
+
+    if (opt !== 0) await pausa();
+  } while (opt !== 0);
+};
+
+main();
---
+++ models/busquedas.js
@@ -0,0 +1,15 @@
+class Busquedas {
+  historial = ["Córdoba", "Jesùs María", "Barcelona"];
+
+  constructor() {
+    //TODO: LEER DB SI EXISTE
+  }
+
+  async ciudad(lugar = "") {
+    //Peticion http
+
+    return []; //Lugares que coincidan
+  }
+}
+
+module.exports = Busquedas;
